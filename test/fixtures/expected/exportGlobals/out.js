46e9f74a89837aeed358296c4b51bf9733db83d8
Philipp A <flying-sheep@web.de>
2023-01-17 16:47:47 -0500

    Update out.js

--- test/fixtures/expected/exportGlobals/out.js
+++ test/fixtures/expected/exportGlobals/out.js
@@ -1,8 +1,9 @@
+import styleInject from "style-inject";
+
 var global = "global";
 var css_248z = ".global { color: blue } .local { color: red }\n";
 var _in = {"global":"global","local":"local"};
 var stylesheet = ".global { color: blue } .local { color: red }\n";
-
 styleInject(css_248z);
 
 export { _in as default, global, local, stylesheet };
