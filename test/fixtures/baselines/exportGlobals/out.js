54c9fedecd82b12d99f53a4934221b43fe10946c
Philipp A <flying-sheep@web.de>
2023-07-26 17:05:44 +0200

    Switch to pnp (#146)

--- test/fixtures/baselines/exportGlobals/out.js
+++ test/fixtures/baselines/exportGlobals/out.js
@@ -2,9 +2,9 @@ import styleInject from 'style-inject';
 
 var global = "global";
 var local = "local";
-var css_248z = ".global { color: blue } .local { color: red }\n";
+var css_248z = ".global {\n  color: blue;\n}\n.local {\n  color: red;\n}\n";
 var _in = {"global":"global","local":"local"};
-var stylesheet=".global { color: blue } .local { color: red }\n";
+var stylesheet=".global {\n  color: blue;\n}\n.local {\n  color: red;\n}\n";
 styleInject(css_248z);
 
 export { _in as default, global, local, stylesheet };
